ead42dff48d5141f5e227435995240186387de4a
chandanaedi <129028757+chandanaedi@users.noreply.github.com>
2026-01-12 19:09:22 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,14 @@
+// config.js - Add your actual Firebase config here
+const firebaseConfig = {
+    apiKey: "AIzaSyCgZXx3pGXIFiN06_c_ZiB-Pi8j6RUtebs",
+    authDomain: "wickreturntracking.firebaseapp.com",
+    databaseURL: "https://wickreturntracking-default-rtdb.firebaseio.com",
+    projectId: "wickreturntracking",
+    storageBucket: "wickreturntracking.firebasestorage.app",
+    messagingSenderId: "917407325282",
+    appId: "1:917407325282:web:9e0f9dc199c286de0a853b",
+    measurementId: "G-DKSLDB8XK5"
+};
+
+// Make it globally available
+window.firebaseConfig = firebaseConfig;
